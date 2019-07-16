87a1123adee81dcb9ff7756f1ca03b7d59b447ed
maxroslow <brian2009@yandex.ru>
2019-07-16 19:46:49 +0400

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { Home, Login, Register } from "pages/";
+import { Navbar } from "components/";
 
 function App() {
   return (
     <div className="App">
-      <h1>Our fiels</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </Router>
     </div>
   );
 }
